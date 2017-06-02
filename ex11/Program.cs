dfe792eafb5ce102499ef645bc8866266d461fcb
murabye <vlada_kuz2011@mail.ru>
2017-06-02 14:22:20 +0500

    ввод шифровки

--- ex11/Program.cs
+++ ex11/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLib;
 
 namespace ex11
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            while (true)
+            {
+                var count = Ask.Num("Введите число k: ");
+                var places = new int[count];
+
+                for (var i = 0; i < count; i++)
+                    places[i] = Ask.Num("Введите место для символа: ");
+
+                var choise = new Menu("Что вы хотели бы сделать: ", "Зашифровать", "Дешифровать");
+
+
+                OC.Stay();
+            }
+
         }
     }
 }
